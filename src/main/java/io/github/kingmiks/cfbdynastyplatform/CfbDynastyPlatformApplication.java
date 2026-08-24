1facb0f65d10f10b9b9b1c5cd09d7a1969fd6dff
KingMiks <Miko.gaddi27@gmail.com>
2026-08-24 11:46:50 -0400

    Initialize Spring Boot setup and first web endpoint

---
+++ src/main/java/io/github/kingmiks/cfbdynastyplatform/CfbDynastyPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.kingmiks.cfbdynastyplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfbDynastyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfbDynastyPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/kingmiks/cfbdynastyplatform/CfbDynastyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kingmiks.cfbdynastyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfbDynastyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
